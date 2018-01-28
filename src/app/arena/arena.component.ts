de2abc28e5021c839c59f1297c9d85819ebca18b
Achal Bhute <aachalbhute@gmail.com>
2018-01-29 00:54:08 +0530

    bug fix

--- src/app/arena/arena.component.ts
+++ src/app/arena/arena.component.ts
@@ -9,35 +9,19 @@ import { Component, OnInit, Output, Input } from '@angular/core';
   styleUrls: ['./arena.component.css']
 })
 export class ArenaComponent implements OnInit {
-  count = 0;
+  
   width = 10;
   height = 10;
-  columns;
-  rows;
   princy = {};
+  count;reset;
+  columns;rows;
+  
   marioX; marioY;
   constructor() {
   }
 
   ngOnInit() {
-    this.width = +prompt('Please enter board width', "10");
-    if (!isNaN(this.width)) {
-      this.height = +prompt('Please enter board height', "10");
-    }
-    else {
-      alert("Please enter number");
-    }
-
-    this.marioX = this.getRandomInt(this.width);
-    this.marioY = this.getRandomInt(this.height);
-    var no_of_princess = (this.height * this.width) / 8;
-    for (var i = 0; i < no_of_princess; i++) {
-      var a = {};
-      a[this.getRandomInt(this.height) + "," + this.getRandomInt(this.width)] = true;
-      this.princy = Object.assign(this.princy, a);
-    }
-    this.columns = new Array(this.height);
-    this.rows = new Array(this.width);
+    this.startGame();
   }
 
   getRandomInt(max) {
@@ -48,43 +32,67 @@ export class ArenaComponent implements OnInit {
   }
 
   hotkeys(event) {
-    switch (event.keyCode) {
-      case 37:
-        if (this.marioX != 0) {
-          this.marioX--;
-          this.count++;
+    if(this.reset==1)
+        this.startGame();
+    else{
+        switch (event.keyCode) {
+          case 37:
+            if (this.marioX != 0) {
+              this.marioX--;
+              this.count++;
+            }
+            break;
+          case 39:
+            if (this.marioX != this.width - 1) {
+              this.marioX++;
+              this.count++;
+            }
+            break;
+          case 38:
+            if (this.marioY != 0) {
+              this.marioY--;
+              this.count++;
+            }
+            break;
+          case 40:
+            if (this.marioY != this.height - 1) {
+              this.marioY++;
+              this.count++;
+            }
+            break;
         }
-        break;
-      case 39:
-        if (this.marioX != this.width - 1) {
-          this.marioX++;
-          this.count++;
-        }
-        break;
-      case 38:
-        if (this.marioY != 0) {
-          this.marioY--;
-          this.count++;
-        }
-        break;
-      case 40:
-        if (this.marioY != this.height - 1) {
-          this.marioY++;
-          this.count++;
-        }
-        break;
-    }
-
-    this.princyCheck();
+        this.princyCheck();
+      }
   }
   princyCheck() {
     if (this.princy[this.marioX + "," + this.marioY] == true) {
       delete this.princy[this.marioX + "," + this.marioY];
     }
-
     if (Object.keys(this.princy).length == 0) {
       alert('Game Over! Total moves to save princess :' + this.count);
+      this.reset=1;
     }
-
+  }
+  startGame(){
+    this.count=0;
+    this.width = +prompt('Please enter board width', "10");
+    if (!isNaN(this.width)) {
+      this.height = +prompt('Please enter board height', "10");
+    }
+    else {
+      alert("Please enter number");
+    }
+      this.reset=0;
+    this.marioX = this.getRandomInt(this.width);
+    this.marioY = this.getRandomInt(this.height);
+    var no_of_princess = (this.height * this.width) / 8;
+    for (var i = 0; i < no_of_princess; i++) {
+      var a = {};
+      a[this.getRandomInt(this.height) + "," + this.getRandomInt(this.width)] = true;
+      this.princy = Object.assign(this.princy, a);
+    }
+    this.princy[this.marioX + "," + this.marioY]?delete this.princy[this.marioX + "," + this.marioY]:"";
+    this.columns = new Array(this.height);
+    this.rows = new Array(this.width);
   }
 }
